b13a943b0b9dcaea6af485e741f3ee34c236d914
Bob Van Landuyt <bob@vanlanduyt.co>
2014-10-03 11:01:32 +0200

    Basic implementation of writing config files

---
+++ lib/wak/package/configuration/file.rb
@@ -0,0 +1,41 @@
+module Wak
+  module Package
+    module Configuration
+      class File
+        attr_accessor :file
+
+        def initialize(path)
+          raise Wak::MissingConfigFile.new("The config file #{path} is missing") if !::File.exists?(path)
+          @file = ::File.new(path)
+        end
+
+        def file_content
+          file.read.split("\n")
+        end
+
+        def build_new_config(lines, after = "")
+          lines =  Array(lines)
+          new_content = []
+          new_content += lines if file_content.empty?
+          file_content.each do |line|
+            new_content << line
+            new_content += lines if line.strip == after
+          end
+          new_content
+        end
+
+        def has_config?(lines)
+          lines = Array(lines)
+          (lines & file_content.map(&:strip)) == lines
+        end
+
+        def write_config(lines, after = "")
+          return true if has_config?(lines)
+          ::File.open(file, "w") do |f|
+            f.write(build_new_config(lines, after).join("\n"))
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/package/configuration/file_test.rb
@@ -0,0 +1,37 @@
+require "helper"
+
+describe Wak::Package::Configuration::File do
+  it "raises an error if a config file is missing" do
+    ::File.expects(:exists?).with("nonexistant").returns(false)
+    assert_raises Wak::MissingConfigFile do
+      Wak::Package::Configuration::File.new("nonexistant")
+    end
+  end
+
+  describe "config" do
+    before do
+      @tempfile = Tempfile.new("waktest")
+      @config_file = Wak::Package::Configuration::File.new(@tempfile.path)
+    end
+
+    it "can add lines to the content" do
+      @config_file.expects(:file_content).returns([""]).twice
+      assert_equal ["", "hello world"], @config_file.build_new_config("hello world")
+    end
+
+    it "can add lines to the content after a specified line" do
+      @config_file.expects(:file_content).returns(["", "   test    ", "empty"]).twice
+      assert_equal ["", "   test    ", "hello world", "empty"], @config_file.build_new_config("hello world", "test")
+    end
+
+    it "can detect if the config was already added" do
+      @config_file.expects(:file_content).returns(["  this is my config"])
+      assert @config_file.has_config?("this is my config")
+    end
+
+    it "can write the config to a file" do
+      @config_file.write_config(["hello", "world"])
+      assert_equal "hello\nworld", ::File.read(@tempfile.path)
+    end
+  end
+end
